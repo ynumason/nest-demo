ae847c05edc45da9524c6f7e48f8acb71dfa122e
布庭 <lizilongdev@gmail.com>
2025-10-04 13:05:56 +0800

    feat(user): 新增用户实体类映射数据库表结构
    - 创建 User 实体类映射 user_database 表
    - 定义主键自增字段 id，类型为 int
    - 添加唯一用户名字段 username，非空且长度限制为50
    - 添加密码字段 password，非空且长度限制为100
    - 添加 isDel 字段表示软删除状态，默认值为0
    - 增加创建时间 createAt 和更新时间 updateAt 字段，自动维护时间戳

---
+++ src/modules/user/user.emtity.ts
@@ -0,0 +1,28 @@
+import { Column, CreateDateColumn, Entity, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
+
+@Entity({ name: 'user_database' })
+// name 表名 配置映射到数据库表名 不指定以实体类名字创建数据表
+
+export class User {
+  // 主键自增 name一致可不写
+  @PrimaryGeneratedColumn({ type: 'int', name: 'id', comment: '主键id' })
+  id: number;
+
+  @Column({ type: 'varchar', length: 50, unique: true, name: 'username', comment: '用户名', nullable: false })
+  username: string;
+
+  @Column({ type: 'varchar', length: 100, name: 'password', comment: '密码', nullable: false })
+  password: string;
+
+  @Column('tinyint', { name: 'is_del', comment: '是否删除,1表示删除,0表示正常', default: () => 0 })
+  isDel: number
+
+  @CreateDateColumn()
+  createAt: Date;
+
+  @UpdateDateColumn()
+  updateAt: Date;
+}
+
+
+
